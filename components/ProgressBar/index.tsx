41a48e0443ce06a706de15ba61be14b753a8e4ce
Avdeev Andrey <avdandrejjj@yandex.ru>
2023-05-16 13:36:26 +0300

    Add
    icon props in Progressbar component

--- components/ProgressBar/index.tsx
+++ components/ProgressBar/index.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Image from "next/image";
+
 import style from "../ProgressBar/style.module.css";
 
 interface progressProps {
   title: string | "";
   percent: number;
+  icon?: string;
 }
 
-export default function ProgressBar({ title, percent }: progressProps) {
+export default function ProgressBar({ title, percent, icon }: progressProps) {
   const [progress, setProgress] = useState<number>(0);
 
   useEffect(() => {
@@ -26,7 +29,12 @@ export default function ProgressBar({ title, percent }: progressProps) {
   return (
     <div className="w-full">
       <div className="flex justify-between">
-        <h1 className="text-white font-bold text-lg">{title}</h1>
+        <div className="flex items-center ">
+          {icon && (
+            <Image className="mr-2" src={icon} alt="html logo" width={18} height={18} />
+          )}
+          <h1 className="text-white font-bold text-lg">{title}</h1>
+        </div>
         <p className="text-gray-200 text-sm">{progress + "%"}</p>
       </div>
       <div className="w-full flex items-center h-4 border-2 border-solid border-gray-400 mt-1 rounded-full">
