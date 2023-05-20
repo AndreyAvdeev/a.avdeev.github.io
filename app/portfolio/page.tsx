24870dd97fb79f2cb5294d3ed7aa0cbc304d2ba3
Avdeev Andrey <avdandrejjj@yandex.ru>
2023-05-20 20:18:58 +0300

    Add
    portfolio

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,11 +1,6 @@
 "use client";
 
-import Image from "next/image";
-import ProgressBar from "@/components/ProgressBar";
-
-import homeIcon from "@/public/home.svg";
-import monitorIcon from "@/public/monitor.svg";
-import penIcon from "@/public/pen.svg";
+import CardImage from "@/components/CardImage";
 
 export default function Portfolio() {
   return (
@@ -15,12 +10,69 @@ export default function Portfolio() {
           Portfolio
         </h1>
       </div>
-      <div className="grid grid-cols-2 mx-20 my-6 ">
+      <div className="mx-20 my-6 ">
         <article>
-        <h2 className="text-white font-bold text-2xl text-left">
-            Gallery
-          </h2>
+          <h2 className="text-white font-bold text-2xl text-left">Gallery</h2>
           <div className="h-0.5 bg-gray-450 w-16 before:content-[''] before:block before:h-0.5 before:w-7 before:bg-blue"></div>
+          <div className="grid justify-items-center mt-8 gap-12 sm:grid-cols-1 md:grid-cols-2  lg:grid-cols-2 xl:grid-cols-3  2xl:grid-cols-5">
+            <div>
+              <CardImage
+                preview={require("@/public/preview/raidofinance_site_prev.png")}
+                images={[
+                  "gallery/raidofinance_site_1.png",
+                  "gallery/raidofinance_site_2.png",
+                  "gallery/raidofinance_site_3.png",
+                  "gallery/raidofinance_site_4.png",
+                  "gallery/raidofinance_site_5.png",
+                  "gallery/raidofinance_site_6.png",
+                ]}
+                hoverElements={[
+                  {
+                    title: "Website",
+                    link: "https://raidofinance.eu/",
+                    target: "_blank",
+                  },
+                  {
+                    title: "Gallery",
+                  },
+                ]}
+              />
+            </div>
+            <div>
+              <CardImage
+                preview={require("@/public/preview/raidowallet_prev.png")}
+                images={["gallery/raidowallet_1.png"]}
+                hoverElements={[
+                  {
+                    title: "Website",
+                    link: "https://raidowallet.com",
+                    target: "_blank",
+                  },
+                  { title: "Gallery" },
+                ]}
+              />
+            </div>
+            <div>
+              <CardImage
+                preview={require("@/public/preview/raido-acquiring_prev.png")}
+                images={[
+                  "gallery/raido-acquiring_1.png",
+                  "gallery/raido-acquiring_2.png",
+                  "gallery/raido-acquiring_3.png",
+                  "gallery/raido-acquiring_4.png",
+                  "gallery/raido-acquiring_5.png",
+                ]}
+                hoverElements={[
+                  {
+                    title: "Website",
+                    link: "https://new-acquiring.raidofinance.eu",
+                    target: "_blank",
+                  },
+                  { title: "Gallery" },
+                ]}
+              />
+            </div>
+          </div>
         </article>
       </div>
     </section>
