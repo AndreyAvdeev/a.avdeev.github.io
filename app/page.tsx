758dfa1421e8dfd80a03ac09a0405e0192f5f003
Avdeev Andrey <avdandrejjj@yandex.ru>
2023-05-21 23:53:25 +0300

    Fix
    responsive main page

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,11 @@ export default function Page() {
 
   return (
     <div className="flex items-center justify-center h-screen px-4">
-      <div className="flex items-center justify-center h-screen grid grid-cols-[0.8fr,1fr] items-center justify-items-center w-11/12 m-auto max-w-7xl">
-        <div className="rounded-full bg-gray-500 shadow-[0px_0px_50px_rgba(0,0,0,0.8)] h-80 w-80 flex items-center justify-center">
+      <div className="grid grid-cols-1 items-center justify-items-center w-11/12 m-auto max-w-7xl lg:grid-cols-[0.8fr,1fr]">
+        <div className="rounded-full bg-gray-500 shadow-[0px_0px_50px_rgba(0,0,0,0.8)] h-80 w-80 flex items-center justify-center mb-6 lg:mb-0">
           <Image src={meIcon} alt="logo" height={290} width={290} />
         </div>
-        <div className="pr-10 md:w-50 lg:w-100 xl:w-30">
+        <div className="flex flex-col  pr-0 md:w-50 lg:w-100 xl:w-30 text-center lg:text-left lg:pr-10">
           <p className="font-poppins text-lg text-gray-300">
             Full Stack Developer
           </p>
@@ -26,7 +26,7 @@ export default function Page() {
             má. Kdyby by Vás mé portofilo přípradně zkušenosti zaujaly - napište
             mi !
           </p>
-          <div className="mt-4">
+          <div className="mt-8 lg:mt-4">
             <button
               className="bg-gray-400 text-white py-2 px-10 rounded-full border-2 border-solid border-[#D5D5D5] hover:border-blue cursor-pointer transition"
               onClick={() => router.push("/portfolio")}
