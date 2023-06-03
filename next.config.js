fa3302e2c0f4cd5797c6a19ac5753edbefd6f19d
Andrey Avdeev <Avdandrejjj@yandex.ru>
2023-06-03 16:35:11 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   output: 'export',
   images: {
-    loader: "akamai",
-    path: "",
-    unoptimized: isProd ? false : true,
+//     loader: "akamai",
+//     path: "",
+    unoptimized: true,
   },
   assetPrefix: './',
   basePath: '',
